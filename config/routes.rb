9515d8fb8084e4194265b1a76594ed3f13f96292
DJ Chung <djchung3@gmail.com>
2011-07-15 21:42:16 -0700

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 SampleApp::Application.routes.draw do
 
+  get "users/new"
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+  match '/signup',  :to => 'users#new'
 
 
   # The priority is based upon order of creation:
